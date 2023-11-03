82ff41a7cf7d1e30c5bfaf35279b54a2d242db53
Юлия Юркина <parma2903@gmail.com>
2023-11-03 11:58:03 +0100

    Разбивает на модули

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,27 @@
-function isStringLengthFits(checkingString, numberOfSymbols) {
-  return checkingString.length <= numberOfSymbols;
-}
-isStringLengthFits('Проверяемая строка', 20);
-isStringLengthFits('Проверяемая строка', 18);
-isStringLengthFits('Проверяемая строка', 10);
+// function isStringLengthFits(checkingString, numberOfSymbols) {
+//   return checkingString.length <= numberOfSymbols;
+// }
+// isStringLengthFits('Проверяемая строка', 20);
+// isStringLengthFits('Проверяемая строка', 18);
+// isStringLengthFits('Проверяемая строка', 10);
 
-function isChekingStringPalindrom(checkingString) {
-  let correctString = checkingString.toString().replace(/\s/g, '').toLowerCase();
-  let emptyString = '';
-  for (i=correctString.length-1; i >= 0; i--) {
-    emptyString += correctString[i];
-  }
-  return emptyString === correctString;
-}
-isChekingStringPalindrom('дОвод');
-isChekingStringPalindrom('Кекс');
-isChekingStringPalindrom('Лёша на полке клопа нашёл ');
+// function isChekingStringPalindrom(checkingString) {
+//   let correctString = checkingString.toString().replace(/\s/g, '').toLowerCase();
+//   let emptyString = '';
+//   for (i=correctString.length-1; i >= 0; i--) {
+//     emptyString += correctString[i];
+//   }
+//   return emptyString === correctString;
+// }
+// isChekingStringPalindrom('дОвод');
+// isChekingStringPalindrom('Кекс');
+// isChekingStringPalindrom('Лёша на полке клопа нашёл ');
 
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
+// const getRandomInteger = (a, b) => {
+//   const lower = Math.ceil(Math.min(a, b));
+//   const upper = Math.floor(Math.max(a, b));
+//   const result = Math.random() * (upper - lower + 1) + lower;
+//   return Math.floor(result);
+// };
 
-getRandomInteger(3,8)
+// getRandomInteger(3,8)
